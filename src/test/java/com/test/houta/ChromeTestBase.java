b70f7e3c5d0979e79c41b06870b6293a178df87c
s-marsi <soufianemarsi1999@gmail.com>
2025-05-24 19:35:04 +0100

    feat: add ChromeTestBase with Chrome WebDriver setup and teardown using @BeforeMethod/@AfterMethod

---
+++ src/test/java/com/test/houta/ChromeTestBase.java
@@ -0,0 +1,27 @@
+package com.test.houta;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+
+public class ChromeTestBase {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
